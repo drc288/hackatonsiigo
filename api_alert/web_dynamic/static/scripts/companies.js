4840e60f6b1b1be2dc1799999a81b7ca2a6a4df5
guxal <jonathanacp93@gmail.com>
2020-02-23 17:26:42 +0000

    add scripts and delete jquery min to template

--- api_alert/web_dynamic/static/scripts/companies.js
+++ api_alert/web_dynamic/static/scripts/companies.js
@@ -1,8 +1,8 @@
-const url = 'http://192.168.33.100:5000/api/v1';
+const url = 'http://192.168.33.100:5001/api/v1';
 
 
 function inputCompaniesHtml(companie) {
-	const html = ['<option value="'+companie.id+'">', companie.name, '</option>'];
+	const html = ['<option value="'+companie.company_id+'">', companie.name, '</option>'];
 
 	return html.join('');
 }
@@ -31,8 +31,47 @@ function getCompanies() {
 	})
 }
 
-function init(){
-	
+function htmlProducts(product) {
+  console.log(product);
+  const html = ['<tr>',
+                '<td>',product.name,'</td>', 
+                '<td><input type="number" data-id="'+product.product_id+'"></td>',
+                '<td>','<button type="submit" class="btn btn-primary mb-2">Add</button>','</td>',
+                '</tr>']
+
+  return html.join('');
 }
 
-$(document).ready(init)
+function addProducts(products) {
+  products.forEach(function(product){
+    $('TBODY').append(htmlProducts(product));
+  })
+}
+
+function getProducts(company_id) {
+  const request = {
+    url: url + '/company/'+company_id+'/products'
+  }
+  const r = $.ajax(request);
+
+  r.done(function(data) {
+    $('TBODY').html('');
+    addProducts(data);
+  })
+
+  r.fail(function(data) {
+
+  })
+}
+
+function init(){
+  console.log('init');
+  getCompanies();
+}
+
+$(document).on('change', 'SELECT', function(){
+  console.log($(this).val());
+  getProducts($(this).val());
+});
+
+$(document).ready(init);
