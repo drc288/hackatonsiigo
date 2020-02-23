a9a9a90b69584fafbe8880d12f321498a7672f9d
guxal <jonathanacp93@gmail.com>
2020-02-23 20:13:29 +0000

    add new routes and create and dismiss save new quantiry

--- api_alert/api/v1/views/index.py
+++ api_alert/api/v1/views/index.py
@@ -7,13 +7,14 @@ from flask import jsonify, request, abort, make_response
 import requests
 import time
 
+
 @app_views.route('/status', methods=["GET"])
 def json_status():
     """
     return a json file
     """
-    
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/companies', methods=["GET"], strict_slashes=False)
 def companies():
@@ -22,19 +23,32 @@ def companies():
     return: a JSON
     """
     res = requests.get('http://0.0.0.0:5002/companies')
-  
     return jsonify(res.json())
 
-@app_views.route('/company/<id>/products',methods=["GET"], strict_slashes=False)
+
+@app_views.route('/company/<id>/products', methods=["GET"],
+                 strict_slashes=False)
 def companies_by_id(id):
     """
     it will list companies by ide
     Return:
     """
-    #print(id)
-    #return 'hola'
     res = requests.get('http://0.0.0.0:5002/company/{}/products'.format(id))
-    #return res
     return jsonify(res.json())
-    
-    
+
+
+@app_views.route('/invoice', methods=["POST"],
+                 strict_slashes=False)
+def invoice():
+    """
+
+    """
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+
+    for d in data:
+        print(d)
+
+    print('hola')
+    return 'hola'
