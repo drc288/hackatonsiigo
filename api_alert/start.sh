ce9f030a4b74360ec0386bc73ea50dcbfcda9cbb
David Rosero C <davidroserocalle@gmail.com>
2020-02-23 15:48:00 +0000

    Open port 5000 and 5001, create the templates to companies

--- api_alert/start.sh
+++ api_alert/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This scipt execute all the services
 # run api alert
-gunicorn -b 0.0.0.0:5000 api.v1.app:app &> /dev/null &
+gunicorn -b 0.0.0.0:5001 api.v1.app:app &> /dev/null &
--- api_alert/web_dynamic/app.py
+++ api_alert/web_dynamic/app.py
@@ -8,22 +8,15 @@ from flask import Flask, render_template, url_for
 # flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-port = 5001
+port = 5000
 host = '0.0.0.0'
 
 
 
-@app.route('/registration')
-def register():
-    return render_template('Registro.html')
+@app.route('/')
+def companies():
+    return render_template('companies.html')
 
-@app.route('/project')
-def project():
-    return render_template('cheker1.html')
-
-@app.route('/tasks')
-def tasks():
-    return render_template('cheker2.html')
 
 if __name__ == "__main__":
     """
